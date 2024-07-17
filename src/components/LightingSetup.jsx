0f6c0ff944f5c437c9434881742dde063bd4b45c
Gloria Lim <glorialimartt@gmail.com>
2024-07-17 01:41:10 -0400

    Add useEffect

--- src/components/LightingSetup.jsx
+++ src/components/LightingSetup.jsx
@@ -1,33 +1,31 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as THREE from 'three';
 
-const LightingSetup = () => {
-  const scene = useScene();
-
-  // Set up lighting
-  const ambientLight = new THREE.AmbientLight(0x404040, 5) // Soft white light
-  scene.add(ambientLight);
-
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-  directionalLight.position.set(0, 7, 2).normalize()
-  scene.add(directionalLight);
+const LightingSetup = ({ scene }) => {
+  useEffect(() => {
+    // Set up lighting
+    const ambientLight = new THREE.AmbientLight(0x404040, 5) // Soft white light
+    scene.add(ambientLight);
   
-  const bottomLight = new THREE.DirectionalLight(0xffffff, 7);
-  bottomLight.position.set(-2, -6, -2).normalize()
-  scene.add(bottomLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+    directionalLight.position.set(0, 7, 2).normalize()
+    scene.add(directionalLight);
+    
+    const bottomLight = new THREE.DirectionalLight(0xffffff, 7);
+    bottomLight.position.set(-2, -6, -2).normalize()
+    scene.add(bottomLight);
+
+    // Clean up lighting
+    return () => {
+      scene.remove(ambientLight);
+      scene.remove(directionalLight);
+      scene.remove(bottomLight);
+    };
+
+  }, [scene]);
 
   return null;
-};
-
-const useScene = () => {
-  const { scene } = useThreeSetup();
-  return scene;
-};  
-
-const useThreeSetup = () => {
-  const scene = new THREE.Scene();
-  return { scene };
 };
 
 export default LightingSetup;
