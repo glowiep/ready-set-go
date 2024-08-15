fd4d525cd0abf7c8a75258d4c67936867011a9bf
Gloria Lim <glorialimartt@gmail.com>
2024-08-14 23:21:38 -0400

    Add brandText

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css'
-import ModelOriginal from './components/ModelOriginal'
 import Model from './components/Model'
-import Scene from './components/Scene'
 
 function App() {
 
   return (
     <div className="canvas-container">
-      {/* <Model /> */}
-      <ModelOriginal />
-      {/* <Scene /> */}
+      <Model />
     </div>
   )
 }
